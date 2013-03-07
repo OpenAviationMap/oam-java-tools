cc281e62808cdfb8f35a3b5c5a3e45f2a90b2ba8
Raymond Raw <rraw@rogers.com>
2013-03-07 00:11:14 +0000

    Fixes a bug resulting from a signature change in the Converter

--- src/test/java/org/openaviationmap/converter/ConverterTest.java
+++ src/test/java/org/openaviationmap/converter/ConverterTest.java
@@ -20,12 +20,6 @@ package org.openaviationmap.converter;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import org.openaviationmap.model.Aerodrome;
-import org.openaviationmap.model.Airspace;
-import org.openaviationmap.model.Boundary;
-import org.openaviationmap.model.Navaid;
-import org.openaviationmap.model.Ring;
-import org.openaviationmap.model.oam.Action;
 
 import java.io.FileInputStream;
 import java.util.List;
@@ -35,6 +29,12 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.junit.Test;
+import org.openaviationmap.model.Aerodrome;
+import org.openaviationmap.model.Airspace;
+import org.openaviationmap.model.Boundary;
+import org.openaviationmap.model.Navaid;
+import org.openaviationmap.model.Ring;
+import org.openaviationmap.model.oam.Action;
 import org.w3c.dom.Document;
 
 /**
@@ -49,44 +49,41 @@ public class ConverterTest {
      */
     @Test
     public void testNationalBorderAirspace() throws Exception {
-        String                  inputFile      = "var/LH-ENR-5.6-en-HU.xml";
-        String                  inputFormat    = "eAIP.Hungary";
-        String                  outputFile     = "var/test-oam-hungary-5.6.xml";
-        String                  outputFormat   = "OAM";
-        Action                  action         = Action.CREATE;
-        String                  borderFile     = "var/hungary.osm";
-        String                  adFile         = "var/LH-AD-1.3-en-HU.xml";
-        int                     version        = 1;
-        List<ParseException>    errors         = new Vector<ParseException>();
+        String inputFile = "var/LH-ENR-5.6-en-HU.xml";
+        String inputFormat = "eAIP.Hungary";
+        String outputFile = "var/test-oam-hungary-5.6.xml";
+        String outputFormat = "OAM";
+        Action action = Action.CREATE;
+        String borderFile = "var/hungary.osm";
+        String adFile = "var/LH-AD-1.3-en-HU.xml";
+        int version = 1;
+        List<ParseException> errors = new Vector<ParseException>();
 
-        Converter.convert(inputFile,
-                          inputFormat,
-                          outputFile,
-                          outputFormat,
-                          action,
-                          borderFile,
-                          adFile,
-                          null,
-                          null,
-                          version,
-                          errors);
+        Converter.convert(inputFile
+                        , inputFormat
+                        , outputFile
+                        , outputFormat
+                        , action
+                        , borderFile
+                        , adFile
+                        , (String) null
+                        , null
+                        , null, version,
+                        errors);
 
         assertTrue(errors.isEmpty());
 
         // so far so good - now read the generated file
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        DocumentBuilder        db  = dbf.newDocumentBuilder();
+        DocumentBuilder db = dbf.newDocumentBuilder();
 
-        Document   d = db.parse(new FileInputStream(outputFile));
-        OAMReader  reader = new OAMReader();
-        List<Airspace>      airspaces  = new Vector<Airspace>();
-        List<Navaid>        navaids    = new Vector<Navaid>();
-        List<Aerodrome>     aerodromes = new Vector<Aerodrome>();
-        reader.processOam(d.getDocumentElement(),
-                          airspaces,
-                          navaids,
-                          aerodromes,
-                          errors);
+        Document d = db.parse(new FileInputStream(outputFile));
+        OAMReader reader = new OAMReader();
+        List<Airspace> airspaces = new Vector<Airspace>();
+        List<Navaid> navaids = new Vector<Navaid>();
+        List<Aerodrome> aerodromes = new Vector<Aerodrome>();
+        reader.processOam(d.getDocumentElement(), airspaces, navaids,
+                aerodromes, errors);
 
         assertTrue(errors.isEmpty());
         assertEquals(37, airspaces.size());
